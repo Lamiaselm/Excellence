5ae66eefd0055b4da9c26f7cc62359a83aafd718
Lamia Selmane <hl_selmane@esi.dz>
2021-02-27 19:40:00 +0100

    accueil navbar done

--- Accueil.php
+++ Accueil.php
@@ -5,11 +5,22 @@ include ('Header.html');
 
 <body>
     <!-- Image and text -->
-<nav class="navbar navbar-light bg-light">
-  <a class="navbar-brand" href="#">
-    <img src="/docs/4.0/assets/brand/bootstrap-solid.svg" width="30" height="30" class="d-inline-block align-top" alt="">
-    Bootstrap
-  </a>
+<nav class="navbar navbar-light bg-light" style="padding:15px;">
+   <div class="row" style="width:100%;"> 
+   <div class="col-md-10">
+   <img src="Assets/images/logo.png" width="180px" height="23px" class="d-inline-block align-top" alt="">
+   </div>
+   <div class="col-md-2" style="float:right;padding-left:120px;"> 
+   <a href="#"><img src="Assets/images/instagram.svg" alt="instagram" width="22px" style="margin-right:7px;"></a>
+   <a href="#"><img src="Assets/images/facebook-circular-logo.svg" alt="instagram" width="22px"style="margin-right:7px;"></a>
+   <a href="#"><img src="Assets/images/twitter-social-logotype.svg" alt="instagram" width="22px"></a>
+
+   </div>
+   </div>
+  
+
+    
+  
 </nav>
 </body>
 
