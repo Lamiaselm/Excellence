cb6c2fc05499479a572ac47922a0d70117362478
Lamia Selmane <hl_selmane@esi.dz>
2021-02-28 19:53:39 +0100

    presentation css done

--- Views/Components/PresentationListe.php
+++ Views/Components/PresentationListe.php
@@ -14,19 +14,23 @@ class PresentationListe {
     public function getPresentationListe()
     {
         echo "
-             <div class='row' style='margin-top:40px;'>
-             <div class='col-md-10'style='margin:0 auto;text-align:center;'>
-             <h1>Présentation d'EXCELLENCE</h1>";
+        <h1 style='text-align:center;margin-top: 30px;font-family: LitSans Medium;'>Présentation d'EXCELLENCE</h1>
+        <form method='POST'action='./Views/PresentationPage.php' style='display:flex;justify-content: center;'> <input type='submit' value='Ajouter un paragraphe' name='paragraphe'style='width:335px;'></form>
+
+             <div class='row' style='margin: 40px auto 0;width: 70%;'>";
+
+             
+             
         $result=$this->presentation->getPresentation();
         foreach($result as $row)
         {
             if($row['image']==null)
             {
                 echo "
-                <div class='col-md-6'style='margin:0 auto;'>
+                <div class='col-md-4'style='margin:0 auto;'>
                               <p>".$row['paragraphe']."</p>
                               <form method='POST' enctype='multipart/form-data'> <input type='file' name='image'> <input type='submit' value='Ajouter une image' name='submit_image'></form>
-                              <form method='POST' action='./Views/PresentationPage.php'> <input type='submit' value='Ajouter un paragraphe' name='submit_file'></form>
+                             
 
                  </div>
                      ";
@@ -38,16 +42,14 @@ class PresentationListe {
 
             }else {
           echo "
-          <div class='col-md-7'style='margin:0 auto;'>
+          <div class='col-md-4'style='margin:0 auto;'>
                         <p>".$row['paragraphe']."</p>
-                        <img src=".$row['image']." alt='icon' style='width: 50px;margin-bottom: 15px;'>
-                        <form method='POST'action='./Views/PresentationPage.php'> <input type='submit' value='Ajouter un paragraphe' name='paragraphe'></form>
-
+                        <img src=".$row['image']." alt='icon' style='width: 100%;margin-bottom: 15px;'>
            </div>
                ";
         }}
         echo "    </div>
-                  </div>
+                 
                   ";
     }
 }
