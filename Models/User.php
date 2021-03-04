8c5b8a8089e617e0e5d7f76dfa15823920590152
Lamia Selmane <hl_selmane@esi.dz>
2021-03-04 14:51:56 +0100

    form register added script input

--- Models/User.php
+++ Models/User.php
@@ -31,6 +31,18 @@ class user{
         $err = Db::execute_query($query);
 
     }
+    public function registerEleve($nom,$prenom,$user_name,$email,$mdp,$date,$adresse,$tel1,$tel2,$tel3,$type,$classe,$parent)
+    {
+        $db =Db::connect();
+        $query1="INSERT INTO users (nom,prenom,user_name,email,mdp,date_naissance,adresse,tel1,tel2,tel3,type) VALUES ('$nom','$prenom','$user_name','$email','$mdp','$date','$adresse','$tel1','$tel2','$tel3','$type')";
+        $err = Db::execute_query($query1);
+        $query2="SELECT ID_user FROM users WHERE email='$email'";
+        return Db::execute_query($query2);
+        $query3="INSERT INTO eleves (ID_classe,ID_parent,ID_user) VALUES";
+        
+       
+
+    }
     public function getUserById($id){
         $requete = "SELECT * FROM users WHERE ID_user = '$id' LIMIT 1";
         return Db::execute_query($requete)[0];
--- Views/RegisterPage.php
+++ Views/RegisterPage.php
@@ -1,6 +1,7 @@
 <?php 
 
 session_start();
+
 ?>
 <!DOCTYPE html>
 <html lang="en">
@@ -15,110 +16,11 @@ session_start();
     <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script>
     <title>Register page</title>
 </head>
-<script>
-  $(document).ready(function(){
-    $("#eleve").click(function(){
-      $("#ul").css("dislay","none");
-      $("#eleve-form").show();
-    });
-    $("#ens").click(function(){
-      $("#ul").hide();
-      $("#ens-form").show();
-    });
-    $("#parent").click(function(){
-      $("#ul").css("dislay","none");
-      $("#parent-form").show();
-    });
-  })
-</script>
+
     <body style="background-color: #494848;">
-    <div class="ul" id="ul" >
-    <ul>
-    <a id="eleve" href="#">  <li>  Ajouter un érudiant </li></a>
-    <a id="ens" href="#">  <li>  Ajouter un enseignant </li></a>
-    <a id="parent" href="#">  <li>  Ajouter un parent </li></a>
-    </ul>
-    </div>
+
     
-     <div class="article-container" style="height:100%;display:none;" id="eleve-form">
-     <h2>Ajouter un utilisateur</h2>
-     <form method="POST" enctype="multipart/form-data">
-  <div class="form-group">
-    <label for="exampleFormControlInput1">Nom de l'utilisateur</label>
-    <input type="text" class="form-control" name="nom" placeholder="Nom">
-  </div>
-  <div class="form-group">
-    <label for="exampleFormControlInput1">Prénom de l'utilisateure</label>
-    <input type="text" class="form-control" name="prenom" placeholder="Prenom">
-  </div>
-  <div class="form-group">
-    <label for="exampleFormControlInput1">Username</label>
-    <input type="text" class="form-control" name="user_name" placeholder="Username">
-  </div>
-  <div class="form-group">
-    <label for="exampleFormControlInput1">Email</label>
-    <input type="email" class="form-control" name="email" placeholder="Email">
-  </div>
-  <div class="form-group">
-    <label for="exampleFormControlInput1">Mot de passe</label>
-    <input type="password" class="form-control" name="mdp" placeholder="Mot de passe">
-  </div>
-  <div class="form-group">
-    <label for="exampleFormControlInput1">Confirmer Mot de passe</label>
-    <input type="password" class="form-control" name="mdp_confirmer" placeholder="Confirmer Mot de passe">
-  </div>
-  <div class="form-group">
-    <label for="exampleFormControlInput1">date de naissance</label>
-    <input type="text" class="form-control" name="date" placeholder="Date daissance">
-  </div>
-  <div class="form-group">
-    <label for="exampleFormControlSelect1">Classe</label>
-    <select class="form-control" name="type">
-      <option value="">Choisir</option>
-      <option value="2">Enseignant</option>
-      <option value="3">Elève</option>
-      <option value="4">Parent</option>
-    </select>
-  </div>
-
-  <div class="form-group">
-    <label for="exampleFormControlInput1">Adresse</label>
-    <input type="text" class="form-control" name="adresse" placeholder="Adresse">
-  </div>
-  <div class="form-group">
-    <label for="exampleFormControlInput1">Numéro de téléphone 1</label>
-    <input type="text" class="form-control" name="tel1" placeholder="Numéro de téléphone 1">
-  </div>
-  <div class="form-group">
-    <label for="exampleFormControlInput1">Numéro de téléphone 2</label>
-    <input type="text" class="form-control" name="tel2" placeholder="Numéro de téléphone 2">
-  </div>
-  <div class="form-group">
-    <label for="exampleFormControlInput1">Numéro de téléphone 3</label>
-    <input type="text" class="form-control" name="tel3" placeholder="Numéro de téléphone 3">
-  </div>
-
-
-  <div class="form-group">
-    <label for="exampleFormControlInput1">Ajouter</label>
-    <input type="submit" class="form-control" name="ajouter"value="ajouter">
-  </div>
-  
-
-</form>
-<?php 
-    require_once __DIR__."/../Controllers/UserController.php";
-    $userCTRL = new UserController();
-    if(isset($_POST["ajouter"])){
-      
-     
-        $userCTRL->register($_POST['nom'],$_POST['prenom'],$_POST['user_name'],$_POST['email'],$_POST['mdp'],$_POST['mdp_confirmer'],$_POST['date'],$_POST['type'],$_POST['adresse'],$_POST['tel1'],$_POST['tel2'],$_POST['tel3']);
-    }
-   
-   
-   ?>
-     </div>
-     <div class="article-container" style="height:100%;display:none;" id="ens-form">
+     <div class="article-container" style="height:100%;">
      <h2>Ajouter un utilisateur</h2>
      <form method="POST" enctype="multipart/form-data">
   <div class="form-group">
@@ -151,14 +53,40 @@ session_start();
   </div>
   <div class="form-group">
     <label for="exampleFormControlSelect1">Type de l'utilisateur</label>
-    <select class="form-control" name="type">
+    <select class="form-control" name="type" id="select">
       <option value="">Choisir</option>
-      <option value="2">Enseignant</option>
-      <option value="3">Elève</option>
-      <option value="4">Parent</option>
+      <option value="2" >Enseignant</option>
+      <option value="3" >Elève</option>
+      <option value="4" >Parent</option>
     </select>
   </div>
-
+  <div class="form-group" id="classe" style="display:none;">
+    <label for="exampleFormControlSelect1">Classes</label>
+    <select class="form-control" name="classe">
+      <option value="">Choisir</option>
+      <option value="2" >2CSSIL1</option>
+      <option value="3" >2CSSIL2</option>
+      <option value="4" >2CSSIQ1</option>
+    </select>
+  </div>
+  <div class="form-group" id="module" style="display:none;">
+    <label for="exampleFormControlSelect1">modules</label>
+    <select class="form-control" name="type">
+      <option value="">Choisir</option>
+      <option value="2" >Outils</option>
+      <option value="3" >Web</option>
+      <option value="4" >Analyse</option>
+    </select>
+  </div>
+  <div class="form-group" id="parent" style="display:none;">
+    <label for="exampleFormControlSelect1">Parents</label>
+    <select class="form-control" name="type">
+      <option value="">Choisir</option>
+      <option value="2" >Outils</option>
+      <option value="3" >Web</option>
+      <option value="4" >Analyse</option>
+    </select>
+  </div>
   <div class="form-group">
     <label for="exampleFormControlInput1">Adresse</label>
     <input type="text" class="form-control" name="adresse" placeholder="Adresse">
@@ -177,9 +105,17 @@ session_start();
   </div>
 
 
-  <div class="form-group">
-    <label for="exampleFormControlInput1">Ajouter</label>
-    <input type="submit" class="form-control" name="ajouter"value="ajouter">
+  <div class="form-group" style="display:none;"id="submit_eleve">
+    <label for="exampleFormControlInput1">Ajouter Eleve</label>
+    <input type="submit" class="form-control" name="ajouter_eleve" value="ajouter eleve" >
+  </div>
+  <div class="form-group"style="display:none;"id="submit_ens">
+    <label for="exampleFormControlInput1">Ajouter Enseignant</label>
+    <input type="submit" class="form-control" name="ajouter_ens" value="ajouter enseignant">
+  </div>
+  <div class="form-group"style="display:none;" id="submit_parent">
+    <label for="exampleFormControlInput1">Ajouter Parent</label>
+    <input type="submit" class="form-control" name="ajouter_parent" value="ajouter parent">
   </div>
   
 
@@ -187,101 +123,72 @@ session_start();
 <?php 
     require_once __DIR__."/../Controllers/UserController.php";
     $userCTRL = new UserController();
-    if(isset($_POST["ajouter"])){
+    if(isset($_POST["ajouter_parent"])){
       
      
         $userCTRL->register($_POST['nom'],$_POST['prenom'],$_POST['user_name'],$_POST['email'],$_POST['mdp'],$_POST['mdp_confirmer'],$_POST['date'],$_POST['type'],$_POST['adresse'],$_POST['tel1'],$_POST['tel2'],$_POST['tel3']);
     }
-   
-   
-   ?>
-     </div>
-     <div class="article-container" style="height:100%;display:none;" id="ens-form">
-     <h2>Ajouter un utilisateur</h2>
-     <form method="POST" enctype="multipart/form-data">
-  <div class="form-group">
-    <label for="exampleFormControlInput1">Nom de l'utilisateur</label>
-    <input type="text" class="form-control" name="nom" placeholder="Nom">
-  </div>
-  <div class="form-group">
-    <label for="exampleFormControlInput1">Prénom de l'utilisateure</label>
-    <input type="text" class="form-control" name="prenom" placeholder="Prenom">
-  </div>
-  <div class="form-group">
-    <label for="exampleFormControlInput1">Username</label>
-    <input type="text" class="form-control" name="user_name" placeholder="Username">
-  </div>
-  <div class="form-group">
-    <label for="exampleFormControlInput1">Email</label>
-    <input type="email" class="form-control" name="email" placeholder="Email">
-  </div>
-  <div class="form-group">
-    <label for="exampleFormControlInput1">Mot de passe</label>
-    <input type="password" class="form-control" name="mdp" placeholder="Mot de passe">
-  </div>
-  <div class="form-group">
-    <label for="exampleFormControlInput1">Confirmer Mot de passe</label>
-    <input type="password" class="form-control" name="mdp_confirmer" placeholder="Confirmer Mot de passe">
-  </div>
-  <div class="form-group">
-    <label for="exampleFormControlInput1">date de naissance</label>
-    <input type="text" class="form-control" name="date" placeholder="Date daissance">
-  </div>
-  <div class="form-group">
-    <label for="exampleFormControlSelect1">Type de l'utilisateur</label>
-    <select class="form-control" name="type">
-      <option value="">Choisir</option>
-      <option value="2">Enseignant</option>
-      <option value="3">Elève</option>
-      <option value="4">Parent</option>
-    </select>
-  </div>
-
-  <div class="form-group">
-    <label for="exampleFormControlInput1">Adresse</label>
-    <input type="text" class="form-control" name="adresse" placeholder="Adresse">
-  </div>
-  <div class="form-group">
-    <label for="exampleFormControlInput1">Numéro de téléphone 1</label>
-    <input type="text" class="form-control" name="tel1" placeholder="Numéro de téléphone 1">
-  </div>
-  <div class="form-group">
-    <label for="exampleFormControlInput1">Numéro de téléphone 2</label>
-    <input type="text" class="form-control" name="tel2" placeholder="Numéro de téléphone 2">
-  </div>
-  <div class="form-group">
-    <label for="exampleFormControlInput1">Numéro de téléphone 3</label>
-    <input type="text" class="form-control" name="tel3" placeholder="Numéro de téléphone 3">
-  </div>
-
-
-  <div class="form-group">
-    <label for="exampleFormControlInput1">Ajouter</label>
-    <input type="submit" class="form-control" name="ajouter"value="ajouter">
-  </div>
+    elseif(isset($_POST["ajouter_eleve"])){
+      
+     
+      $userCTRL->register($_POST['nom'],$_POST['prenom'],$_POST['user_name'],$_POST['email'],$_POST['mdp'],$_POST['mdp_confirmer'],$_POST['date'],$_POST['type'],$_POST['adresse'],$_POST['tel1'],$_POST['tel2'],$_POST['tel3']);
+  }   elseif(isset($_POST["ajouter_ens"])){
+      
+     
+    $userCTRL->register($_POST['nom'],$_POST['prenom'],$_POST['user_name'],$_POST['email'],$_POST['mdp'],$_POST['mdp_confirmer'],$_POST['date'],$_POST['type'],$_POST['adresse'],$_POST['tel1'],$_POST['tel2'],$_POST['tel3']);
+}
   
-
-</form>
-<?php 
-    require_once __DIR__."/../Controllers/UserController.php";
-    $userCTRL = new UserController();
-    if(isset($_POST["ajouter"])){
-      
-     
-        $userCTRL->register($_POST['nom'],$_POST['prenom'],$_POST['user_name'],$_POST['email'],$_POST['mdp'],$_POST['mdp_confirmer'],$_POST['date'],$_POST['type'],$_POST['adresse'],$_POST['tel1'],$_POST['tel2'],$_POST['tel3']);
-    }
    
    
    ?>
      </div>
+   
+   
+    
 
     </body>
 <!-- SCRIPTS-->
-<script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
-<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
-<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
-<script>
 
+<script>
+  
+    
+  
+$(document).ready(function()
+{
+ $("#select").change(function()
+ {
+  if($(this).val() == "2")
+  { $("#classe").hide();
+    $("#parent").hide();
+    $("#parent").hide();
+    $("#submit_eleve").hide();
+    $("#submit_parent").hide();
+    $("#module").show();
+    $("#submit_ens").show();
+
+  }
+  else
+ if($(this).val() == "3") {
+   $("#module").hide();
+   $("#submit_ens").hide();
+    $("#submit_parent").hide();
+   $("#classe").show();
+   $("#parent").show();
+   $("#submit_eleve").show();
+  }
+  else
+ if($(this).val() == "4") {
+  $("#module").hide();
+  $("#classe").hide();
+  $("#parent").hide();
+  $("#submit_eleve").hide();
+  $("#submit_ens").hide();
+  $("#submit_parent").show();
+  }
+ });
+
+});
 </script>
+
 </body>
 </html>
